9201b9bbb1cba7ca5e09bd684db947dd952c52af
SergeyMakarov10 <serjmak@icloud.com>
2024-06-03 20:25:56 +0400

    Обновил urls для обновления данных о перевале

--- fstr_project/pereval/urls.py
+++ fstr_project/pereval/urls.py
@@ -9,6 +9,7 @@ router.register(r'users', UserViewset)
 router.register(r'coordinates', CoordinateViewset)
 router.register(r'levels', LevelViewset)
 router.register(r'images', ImageViewset)
+router.register(r'update_pereval', PerevalUpdateViewset, basename='update_pereval')
 
 urlpatterns = [
     path('', include(router.urls)),
